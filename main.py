e5d57d845ce76a7269bf50f5c4e9a6247232caa9
Eduardoooo0 <el957733@gmail.com>
2024-08-20 10:16:14 -0300

    Substituindo as 3 listas por uma lista de dicionarios

--- main.py
+++ main.py
@@ -5,32 +5,45 @@ app = Flask(__name__)
 
 app.config['tempo_de_expiracao_quiz'] = 62  # 1 minute
 app.config['pergunta_atual'] = 0
+app.config['fase_atual'] = 1
 
-perguntas = ['Quando Felipe tinha 5 anos, o pai dele tinha 36 anos. Agora Felipe tem a metade da idade do pai. Quantos anos Felipe tem?',
-             'Observando a sequência, o número que substitui a interrogação é? 1, 7, 6, – 1, – 7, – 6, 1, 7, 6, ?',
-             'Qual é o sucessor do sucessor de 199?',
-             'Qual é o antecessor do sucessor de 45?',
-             'O valor da expressão (100 - 40) : 3 é:',
-             'Em um anfiteatro, as cadeiras estão dispostas em 20 linhas e 15 colunas. Qual é o número total de cadeiras?',
-             'Carlos guardou sua coleção de latas de refrigerante em casa. Em cada caixa couberam 28 latas. Ele usou 7 caixas e sobraram 6 latas de refrigerante. Quantas latas tem a coleção de Carlos?',
-             'Arrumando 512 cadernos em 12 pacotes de mesma quantidade, sobraram ainda 8 cadernos. Quantos cadernos foram colocados em cada pacote?',
-             'Um granjeiro tem 3 333 ovos para vender. Se colocar 33 ovos em cada caixa, quantas caixas completas vão formar?',
-             'Alvimar pagou uma compra de R$ 3,50 com uma nota de R$ 5,00 e recebeu o troco em moedas de R$ 0,25. Quantas moedas ele recebeu?'
-             ]
+questoes = [
+    {1: [
+            {'perguntas': [
+                    'Quando Felipe tinha 5 anos, o pai dele tinha 36 anos. Agora Felipe tem a metade da idade do pai. Quantos anos Felipe tem?',
+                    'Observando a sequência, o número que substitui a interrogação é? 1, 7, 6, – 1, – 7, – 6, 1, 7, 6, ?',
+                    'Qual é o sucessor do sucessor de 199?',
+                    'Qual é o antecessor do sucessor de 45?',
+                    'O valor da expressão (100 - 40) : 3 é:',
+                    'Em um anfiteatro, as cadeiras estão dispostas em 20 linhas e 15 colunas. Qual é o número total de cadeiras?',
+                    'Carlos guardou sua coleção de latas de refrigerante em casa. Em cada caixa couberam 28 latas. Ele usou 7 caixas e sobraram 6 latas de refrigerante. Quantas latas tem a coleção de Carlos?',
+                    'Arrumando 512 cadernos em 12 pacotes de mesma quantidade, sobraram ainda 8 cadernos. Quantos cadernos foram colocados em cada pacote?',
+                    'Um granjeiro tem 3 333 ovos para vender. Se colocar 33 ovos em cada caixa, quantas caixas completas vão formar?',
+                    'Alvimar pagou uma compra de R$ 3,50 com uma nota de R$ 5,00 e recebeu o troco em moedas de R$ 0,25. Quantas moedas ele recebeu?'
+                ]
+            },
+            {'opcoes': [
+                    ['18 anos', '24 anos', '31 anos', '36 anos'],
+                    ['1', '-7', '6', '-1'],
+                    ['199', '200', '201', '202'],
+                    ['44', '45', '46', '47'],
+                    ['10', '15', '20', '24'],
+                    ['35', '150', '200', '300'],
+                    ['194', '196', '202', '238'],
+                    ['28', '36', '42', '48'],
+                    ['100', '101', '110', '111'],
+                    ['4', '5', '6', '7']
+                ]
+            },
+            {'respostas': [
+                    '31 anos', '-1', '201', '45', '20', '300', '202', '42', '101', '6'
+                ]
+            }
+        ]
+    }
+]
 
-opcoes = [['18 anos', '24 anos', '31 anos', '36 anos'], 
-          ['1', '-7', '6', '-1'], 
-          ['199', '200', '201', '202'],
-          ['44','45','46','47'],
-          ['10','15','20','24'],
-          ['35','150','200','300'],
-          ['194','196','202','238'],
-          ['28','36','42','48'],
-          ['100','101','110','111'],
-          ['4','5','6','7']
-          ]
-
-respostas = ['31 anos', '-1', '201','45','20','300','202','42','101','6']
+             
 
 @app.route('/')
 def Index():
@@ -42,25 +55,25 @@ def Login():
 
 @app.route('/quiz', methods=['POST', 'GET'])
 def Quiz():
+    fase_atual = app.config['fase_atual']
     if request.method == 'POST':
         pergunta_atual = app.config['pergunta_atual']
         # Obter a resposta do usuário
         resposta_user = request.form.get('resposta')
         if resposta_user is None:
-            # Error handling if the user doesn't select any option
             mensagem = "Por favor, selecione uma resposta."
-            response = make_response(render_template('jogo_quiz.html', pergunta=perguntas[pergunta_atual], opcoes=opcoes[pergunta_atual], mensagem=mensagem))
+            response = make_response(render_template('jogo_quiz.html',   pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
             response.set_cookie('pergunta_atual', str(pergunta_atual))
             return response
         else:
             # Verificar se a resposta está correta
-            if resposta_user == respostas[pergunta_atual]:
+            if resposta_user == questoes[fase_atual-1][fase_atual][2]['respostas'][pergunta_atual]:
                 # Passar para a próxima questão
                 pergunta_atual += 1
                 app.config['pergunta_atual'] = pergunta_atual
-                if pergunta_atual < len(perguntas):
+                if pergunta_atual < len(questoes[fase_atual-1][fase_atual][0]['perguntas']):
                     mensagem = "Parabéns! Sua resposta está correta."
-                    response = make_response(render_template('jogo_quiz.html', pergunta=perguntas[pergunta_atual], opcoes=opcoes[pergunta_atual], mensagem=mensagem))
+                    response = make_response(render_template('jogo_quiz.html',   pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
                     response.set_cookie('pergunta_atual', str(pergunta_atual))
                     response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
                     return response
@@ -69,9 +82,9 @@ def Quiz():
             else:
                 pergunta_atual += 1
                 app.config['pergunta_atual'] = pergunta_atual
-                if pergunta_atual < len(perguntas):
-                    mensagem = f"Desculpe, a resposta correta era {respostas[pergunta_atual-1]}."
-                    response = make_response(render_template('jogo_quiz.html', pergunta=perguntas[pergunta_atual], opcoes=opcoes[pergunta_atual], mensagem=mensagem))
+                if pergunta_atual < len(questoes[fase_atual-1]['1'][0]['perguntas']):
+                    mensagem = f"Desculpe, a resposta correta era {questoes[fase_atual][2]['respostas'][pergunta_atual - 1]}."
+                    response = make_response(render_template('jogo_quiz.html',  pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
                     response.set_cookie('pergunta_atual', str(pergunta_atual))
                     response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
                     return response
@@ -81,7 +94,7 @@ def Quiz():
         # Se a página foi atualizada, voltar para a primeira pergunta
         pergunta_atual = 0
         app.config['pergunta_atual'] = pergunta_atual
-        response = make_response(render_template('jogo_quiz.html', pergunta=perguntas[pergunta_atual], opcoes=opcoes[pergunta_atual], mensagem=''))
+        response = make_response(render_template('jogo_quiz.html',  pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual]))
         response.set_cookie('pergunta_atual', str(pergunta_atual))
         response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
         return response
