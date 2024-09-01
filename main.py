0125ecc93b5c82154b871e793565dbb8d957d134
Eduardoooo0 <el957733@gmail.com>
2024-09-01 15:53:04 -0300

    Arrumando os indices

--- main.py
+++ main.py
@@ -12,113 +12,112 @@ app.config['pontuacao'] = 0
 
 bancodados = {}
 
-questoes = [
-    {1: [
-            {'perguntas': [
-                    'Quando Felipe tinha 5 anos, o pai dele tinha 36 anos. Agora Felipe tem a metade da idade do pai. Quantos anos Felipe tem?',
-                    'Observando a sequência, o número que substitui a interrogação é? 1, 7, 6, – 1, – 7, – 6, 1, 7, 6, ?',
-                    'Qual é o sucessor do sucessor de 199?',
-                    'Qual é o antecessor do sucessor de 45?',
-                    'O valor da expressão (100 - 40) : 3 é:',
-                    'Em um anfiteatro, as cadeiras estão dispostas em 20 linhas e 15 colunas. Qual é o número total de cadeiras?',
-                    'Carlos guardou sua coleção de latas de refrigerante em casa. Em cada caixa couberam 28 latas. Ele usou 7 caixas e sobraram 6 latas de refrigerante. Quantas latas tem a coleção de Carlos?',
-                    'Arrumando 512 cadernos em 12 pacotes de mesma quantidade, sobraram ainda 8 cadernos. Quantos cadernos foram colocados em cada pacote?',
-                    'Um granjeiro tem 3 333 ovos para vender. Se colocar 33 ovos em cada caixa, quantas caixas completas vão formar?',
-                    'Alvimar pagou uma compra de R$ 3,50 com uma nota de R$ 5,00 e recebeu o troco em moedas de R$ 0,25. Quantas moedas ele recebeu?'
-                ]
-            },
-            {'opcoes': [
-                    ['18 anos', '24 anos', '31 anos', '36 anos'],
-                    ['1', '-7', '6', '-1'],
-                    ['199', '200', '201', '202'],
-                    ['44', '45', '46', '47'],
-                    ['10', '15', '20', '24'],
-                    ['35', '150', '200', '300'],
-                    ['194', '196', '202', '238'],
-                    ['28', '36', '42', '48'],
-                    ['100', '101', '110', '111'],
-                    ['4', '5', '6', '7']
-                ]
-            },
-            {'respostas': [
-                    '31 anos', '-1', '201', '45', '20', '300', '202', '42', '101', '6'
-                ]
-            }
+fases = [
+            [
+                {
+                    "pergunta": "Quando Felipe tinha 5 anos, o pai dele tinha 36 anos. Agora Felipe tem a metade da idade do pai. Quantos anos Felipe tem?",
+                    "opcoes": ["18 anos", "24 anos", "31 anos", "36 anos"],
+                    "resposta": "31 anos"
+                },
+                {
+                    "pergunta": "Observando a sequência, o número que substitui a interrogação é? 1, 7, 6, – 1, – 7, – 6, 1, 7, 6, ?",
+                    "opcoes": ["1", "-7", "6", "-1"],
+                    "resposta": "-1"
+                },
+                {
+                    "pergunta": "Qual é o sucessor do sucessor de 199?",
+                    "opcoes": ["199", "200", "201", "202"],
+                    "resposta": "201"
+                },
+                {
+                    "pergunta": "Qual é o antecessor do sucessor de 45?",
+                    "opcoes": ["44", "45", "46", "47"],
+                    "resposta": "45"
+                },
+                {
+                    "pergunta": "O valor da expressão (100 - 40) : 3 é:",
+                    "opcoes": ["10", "15", "20", "24"],
+                    "resposta": "20"
+                },
+                {
+                    "pergunta": "Em um anfiteatro, as cadeiras estão dispostas em 20 linhas e 15 colunas. Qual é o número total de cadeiras?",
+                    "opcoes": ["35", "150", "200", "300"],
+                    "resposta": "300"
+                },
+                {
+                    "pergunta": "Carlos guardou sua coleção de latas de refrigerante em casa. Em cada caixa couberam 28 latas. Ele usou 7 caixas e sobraram 6 latas de refrigerante. Quantas latas tem a coleção de Carlos?",
+                    "opcoes": ["194", "196", "202", "238"],
+                    "resposta": "202"
+                },
+                {
+                    "pergunta": "Arrumando 512 cadernos em 12 pacotes de mesma quantidade, sobraram ainda 8 cadernos. Quantos cadernos foram colocados em cada pacote?",
+                    "opcoes": ["28", "36", "42", "48"],
+                    "resposta": "42"
+                },
+                {
+                    "pergunta": "Um granjeiro tem 3 333 ovos para vender. Se colocar 33 ovos em cada caixa, quantas caixas completas vão formar?",
+                    "opcoes": ["100", "101", "110", "111"],
+                    "resposta": "101"
+                },
+                {
+                    "pergunta": "Alvimar pagou uma compra de R$ 3,50 com uma nota de R$ 5,00 e recebeu o troco em moedas de R$ 0,25. Quantas moedas ele recebeu?",
+                    "opcoes": ["4", "5", "6", "7"],
+                    "resposta": "6"
+                }
+            ],
+            [
+                {
+                    "pergunta": "1+1?",
+                    "opcoes": ["1", "2", "3", "4"],
+                    "resposta": "2"
+                },
+                {
+                    "pergunta": "1+2?",
+                    "opcoes": ["1", "3", "2", "5"],
+                    "resposta": "3"
+                },
+                {
+                    "pergunta": "1+3?",
+                    "opcoes": ["1", "3", "4", "5"],
+                    "resposta": "4"
+                },
+                {
+                    "pergunta": "1+4?",
+                    "opcoes": ["1", "3", "2", "5"],
+                    "resposta": "5"
+                },
+                {
+                    "pergunta": "1+5?",
+                    "opcoes": ["1", "6", "2", "5"],
+                    "resposta": "6"
+                },
+                {
+                    "pergunta": "1+6?",
+                    "opcoes": ["7", "3", "2", "5"],
+                    "resposta": "7"
+                },
+                {
+                    "pergunta": "1+7?",
+                    "opcoes": ["1", "8", "2", "5"],
+                    "resposta": "8"
+                },
+                {
+                    "pergunta": "1+8?",
+                    "opcoes": ["1", "3", "2", "9"],
+                    "resposta": "9"
+                },
+                {
+                    "pergunta": "1+9?",
+                    "opcoes": ["1", "3", "10", "5"],
+                    "resposta": "10"
+                },
+                {
+                    "pergunta": "1+10?",
+                    "opcoes": ["11", "3", "2", "5"],
+                    "resposta": "11"
+                }
+            ]
         ]
-    },
-    {2:[
-            {'perguntas':[
-                '1+1?',
-                '1+2?',
-                '1+3?',
-                '1+4?',
-                '1+5?',
-                '1+6?',
-                '1+7?',
-                '1+8?',
-                '1+9?',
-                '1+10?'
-                ]
-
-            },
-            {'opcoes':[
-                ['1','2','3','4'],
-                ['1','3','2','5'],
-                ['1','3','4','5'],
-                ['1','3','2','5'],
-                ['1','6','2','5'],
-                ['7','3','2','5'],
-                ['1','8','2','5'],
-                ['1','3','2','9'],
-                ['1','3','10','5'],
-                ['11','3','2','5']
-            ]
-
-            },
-            {'respostas':[
-                '2','3','4','5','6','7','8','9','10','11'
-            ]
-
-            }
-        ]
-    },
-    {3:[
-            {'perguntas':[
-                '1+2?',
-                '1+1?',
-                '1+3?',
-                '1+4?',
-                '1+5?',
-                '1+6?',
-                '1+7?',
-                '1+8?',
-                '1+9?',
-                '1+10?'
-                ]
-
-            },
-            {'opcoes':[
-                ['1','2','3','4'],
-                ['1','3','2','5'],
-                ['1','3','4','5'],
-                ['1','3','2','5'],
-                ['1','6','2','5'],
-                ['7','3','2','5'],
-                ['1','8','2','5'],
-                ['1','3','2','9'],
-                ['1','3','10','5'],
-                ['11','3','2','5']
-            ]
-
-            },
-            {'respostas':[
-                '3','2','4','5','6','7','8','9','10','11'
-            ]
-
-            }
-        ]
-    }
-]
 
 @app.route('/')
 def Index():
@@ -174,83 +173,85 @@ def Perfil():
 
 @app.route('/quiz', methods=['POST', 'GET'])
 def Quiz():
-    fase_desbloqueada = app.config['fase_desbloqueada']
-    pontuacao = app.config['pontuacao']
-    pergunta_atual = app.config['pergunta_atual']
+    fase_desbloqueada = app.config.get('fase_desbloqueada', 1)
+    pontuacao = app.config.get('pontuacao', 0)
+    pergunta_atual = int(request.cookies.get('pergunta_atual', 0))
+
     if request.method == 'POST':
-        fase_atual = app.config['fase_atual']
+        fase_atual = int(request.cookies.get('fase_atual', 1))
         if fase_atual <= fase_desbloqueada:
-        # Obter a resposta do usuário
             resposta_user = request.form.get('resposta')
             if resposta_user is None:
                 mensagem = "Por favor, selecione uma resposta."
-                response = make_response(render_template('jogo_quiz.html',   pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
+                response = make_response(render_template('jogo_quiz.html', pergunta=fases[fase_atual-1][pergunta_atual]['pergunta'],    opcoes=fases[fase_atual-1][pergunta_atual]['opcoes'],    mensagem=mensagem))
                 response.set_cookie('pergunta_atual', str(pergunta_atual))
+                response.set_cookie('fase_atual', str(fase_atual))
+                response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
                 return response
             else:
-                # Verificar se a resposta está correta
-                if resposta_user == questoes[fase_atual-1][fase_atual][2]['respostas'][pergunta_atual]:
-                    # Passar para a próxima questão
+                if resposta_user == fases[fase_atual-1][pergunta_atual]['resposta']:
                     pontuacao += 1
                     app.config['pontuacao'] = pontuacao
                     pergunta_atual += 1
-                    app.config['pergunta_atual'] = pergunta_atual
-                    if pergunta_atual < len(questoes[fase_atual-1][fase_atual][0]['perguntas']):
+                    if pergunta_atual < len(fases[fase_atual-1]):
                         mensagem = "Parabéns! Sua resposta está correta."
-                        response = make_response(render_template('jogo_quiz.html',   pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
+                        response = make_response(render_template('jogo_quiz.html',pergunta=fases[fase_atual-1][pergunta_atual]['pergunta'],opcoes=fases[fase_atual-1][pergunta_atual]['opcoes'],mensagem=mensagem))
                         response.set_cookie('pergunta_atual', str(pergunta_atual))
                         response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
                         return response
                     else:
                         if pontuacao >= 7:
-                            message = f"Parabéns sua pontuação foi {pontuacao}/{len(questoes[fase_atual-1][fase_atual][0]['perguntas'])}. Pode avançar de fase."
+                            message = f"Parabéns sua pontuação foi {pontuacao}/{len(fases[fase_atual-1])}. Pode avançar de fase."
                             pontuacao = 0
                             app.config['pontuacao'] = pontuacao
                             pergunta_atual = 0
-                            app.config['pergunta_atual'] = pergunta_atual
                             fase_desbloqueada += 1
                             app.config['fase_desbloqueada'] = fase_desbloqueada
-                            return render_template('tela_final.html',mensagem=message)
+                            response = make_response(render_template('tela_final.html', mensagem=message))
+                            response.set_cookie('fase_atual', str(fase_atual + 1))
+                            return response
                         else:
-                            message = f"Sua pontuação foi {pontuacao}/{len(questoes[fase_atual-1][fase_atual][0]['perguntas'])}. Tente fazer uma melhor pontuação para avançar a fase."
+                            message = f"Sua pontuação foi {pontuacao}/{len(fases[fase_atual-1])}. Tente fazer uma melhor pontuação para avançar a fase."
                             pontuacao = 0
                             app.config['pontuacao'] = pontuacao
-                            return render_template('tela_final.html',mensagem=message)
+                            response = make_response(render_template('tela_final.html', mensagem=message))
+                            response.set_cookie('fase_atual', str(fase_atual))
+                            return response
                 else:
                     pergunta_atual += 1
-                    app.config['pergunta_atual'] = pergunta_atual
-                    if pergunta_atual < len(questoes[fase_atual-1][1][0]['perguntas']):
-                        mensagem = f"Desculpe, a resposta correta era {questoes[fase_atual-1][fase_atual][2]['respostas'][pergunta_atual - 1]}."
-                        response = make_response(render_template('jogo_quiz.html',  pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual], mensagem=mensagem))
+                    if pergunta_atual < len(fases[fase_atual-1]):
+                        mensagem = f"Desculpe, a resposta correta era {fases[fase_atual-1][pergunta_atual]['resposta']}."
+                        response = make_response(render_template('jogo_quiz.html',pergunta=fases[fase_atual-1][pergunta_atual]['pergunta'],opcoes=fases[fase_atual-1][pergunta_atual]['opcoes'],mensagem=mensagem))
                         response.set_cookie('pergunta_atual', str(pergunta_atual))
                         response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
                         return response
                     else:
                         if pontuacao >= 7:
-                            message = f"Parabéns sua pontuação foi {pontuacao}/{len(questoes[fase_atual-1][fase_atual][0]['perguntas'])}. Pode avançar de fase."
+                            message = f"Parabéns sua pontuação foi {pontuacao}/{len(fases[fase_atual-1])}. Pode avançar de fase."
                             pontuacao = 0
                             app.config['pontuacao'] = pontuacao
                             pergunta_atual = 0
-                            app.config['pergunta_atual'] = pergunta_atual
                             fase_desbloqueada += 1
                             app.config['fase_desbloqueada'] = fase_desbloqueada
-                            return render_template('tela_final.html',mensagem=message)
+                            response = make_response(render_template('tela_final.html', mensagem=message))
+                            response.set_cookie('fase_atual', str(fase_atual + 1))
+                            return response
                         else:
-                            message = f"Sua pontuação foi {pontuacao}/{len(questoes[fase_atual-1][fase_atual][0]['perguntas'])}. Tente fazer uma melhor pontuação para avançar a fase."
+                            message = f"Sua pontuação foi {pontuacao}/{len(fases[fase_atual-1])}. Tente fazer uma melhor pontuação para avançar a fase."
                             pontuacao = 0
                             app.config['pontuacao'] = pontuacao
-                            return render_template('tela_final.html',mensagem=message)
+                            response = make_response(render_template('tela_final.html', mensagem=message))
+                            response.set_cookie('fase_atual', str(fase_atual))
+                            return response
         else:
             return redirect(url_for('Fases_quiz'))
     else:
-        fase_atual = int(request.args.get('fase', 1))
-        app.config['fase_atual'] = fase_atual
-        if fase_atual <= fase_desbloqueada:               
-            # Se a página foi atualizada, voltar para a primeira pergunta
+        fase_atual = int(request.cookies.get('fase_atual', 1))
+        if fase_atual <= fase_desbloqueada:
             pergunta_atual = 0
-            app.config['pergunta_atual'] = pergunta_atual
-            response = make_response(render_template('jogo_quiz.html',  pergunta=questoes[fase_atual-1][fase_atual][0]['perguntas'][pergunta_atual], opcoes=questoes[fase_atual-1][fase_atual][1]['opcoes'][pergunta_atual]))
+            response = make_response(render_template('jogo_quiz.html',pergunta=fases[fase_atual-1][pergunta_atual]['pergunta'],opcoes=fases[fase_atual-1][pergunta_atual]['opcoes'],mensagem=''))
             response.set_cookie('pergunta_atual', str(pergunta_atual))
+            response.set_cookie('fase_atual', str(fase_atual))
             response.set_cookie('tempo_de_inicio_quiz', str(datetime.now()))
             return response
         else:
