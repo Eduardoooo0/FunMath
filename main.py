4ab2b39282799df1e30693581a2a035662190307
Eduardoooo0 <el957733@gmail.com>
2024-09-25 21:04:53 -0300

    adicionando o local storage

--- main.py
+++ main.py
@@ -417,7 +417,7 @@ def Fases_qcbc():
 @app.route('/jogo_qcbc',methods=['POST', 'GET'])
 def Jogo_qcbc():
     if request.method == 'GET':
-        return render_template('jogo_qcbc.html',nome='')
+        return render_template('jogo_qcbc.html',valor=None)
     
 @app.route('/questoes_qcbc',methods=['POST', 'GET'])
 def Questoes_qcbc():
@@ -431,7 +431,7 @@ def Questoes_qcbc():
         resposta = request.form.get('resposta')
         questao = int(request.cookies.get('questao_atual'))
         if resposta == fases[questao-1][questao-1]['resposta']:
-            response = make_response(render_template('jogo_qcbc.html',nome=questao))
+            response = make_response(render_template('jogo_qcbc.html',valor=questao))
             response.set_cookie('questao_atual',str(questao))
             return response
         
