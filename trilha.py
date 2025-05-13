6e44fbc0d618a317ede80432f7d1ff1ca58b347f
késsia <ylannekessia29@gmail.com>
2025-05-13 10:38:06 -0300

    fotos do quebra-cabeça atualizadas

--- trilha.py
+++ trilha.py
@@ -2,39 +2,39 @@
 
 fases_trilha = [
                 {
-                    "pergunta": "../static/imgs/questao1_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao1_trilha.png",
                     "resposta": "5"
                 },
                 {
-                    "pergunta": "../static/imgs/questao2_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao2_trilha.png",
                     "resposta": "7"
                 },
                 {
-                    "pergunta": "../static/imgs/questao3_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao3_trilha.png",
                     "resposta": "2"
                 },
                 {
-                    "pergunta": "../static/imgs/questao4_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao4_trilha.png",
                     "resposta": "4"
                 },
                 {
-                    "pergunta": "../static/imgs/questao5_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao5_trilha.png",
                     "resposta": "3"
                 },
                 {
-                    "pergunta": "../static/imgs/questao6_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao6_trilha.png",
                     "resposta": "1"
                 },
                 {
-                    "pergunta": "../static/imgs/questao7_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao7_trilha.png",
                     "resposta": "9"
                 },
                 {
-                    "pergunta": "../static/imgs/questao8_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao8_trilha.png",
                     "resposta": "6"
                 },
                 {
-                    "pergunta": "../static/imgs/questao9_trilha.png",
+                    "pergunta": "../static/imgs/questoes_trilha/questao9_trilha.png",
                     "resposta": "8"
                 }]
 
